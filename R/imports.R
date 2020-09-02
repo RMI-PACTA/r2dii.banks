33517a7e60585df3ae42b7372ce16866f2525147
Mauro Lepore <maurolepore@gmail.com>
2020-09-02 16:10:10 -0500

    Add install.R

---
+++ R/imports.R
@@ -0,0 +1,5 @@
+# Avoid R CMD check NOTE
+#' @import r2dii.data
+#' @importFrom r2dii.match crucial_lbk
+#' @importFrom r2dii.analysis target_sda
+NULL
---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("shiny")
+install.packages("remotes")
+install_github("2DegreesInvesting/r2dii.banks")
