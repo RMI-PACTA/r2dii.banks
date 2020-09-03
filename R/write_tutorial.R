171d03b0c35c6a5ad9fe3c2a20916ae5dff262d4
Mauro Lepore <maurolepore@gmail.com>
2020-09-03 13:32:44 -0500

    Use learnr options

--- R/write_tutorial.R
+++ R/write_tutorial.R
@@ -1,14 +1,22 @@
+write_tutorials <- function(.x = c("data", "match", "analysis")) {
+  lapply(.x, write_tutorial)
+}
+
 write_tutorial <- function(.x) {
-  suffix <- function(x, y) sprintf("%s%s", x, y)
-
   yaml <- sub("(r2dii.)package", suffix("\\1", .x), readLines(rmd("yaml.Rmd")))
-
   readme <- readme_from(
     "maurolepore", suffix("r2dii.", .x), branch = "label-chunks"
   )
-  readme <- sub("^(```\\{.*)\\}", "\\1, exercise=TRUE}", readme)
 
-  lines <- c(yaml, "\n", readLines(rmd("setup.Rmd")), readme)
+  lines <- c(
+    yaml,
+    "\n",
+    readLines(rmd("setup.Rmd")),
+    "\n",
+    "## Description",
+    "\n",
+    readme
+  )
 
   directory <- suffix("intro-r2dii-", .x)
   file <- sprintf("intro-r2dii-%s.Rmd", .x)
@@ -17,6 +25,8 @@ write_tutorial <- function(.x) {
 
   invisible(.x)
 }
+
+suffix <- function(x, y) sprintf("%s%s", x, y)
 
 rmd <- function(file) {
   system.file("tutorials", file, mustWork = TRUE, package = "r2dii.banks")
