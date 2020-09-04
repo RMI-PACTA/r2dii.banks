bde97589a7fba68b44062865c48919d50c1cf229
Mauro Lepore <maurolepore@gmail.com>
2020-09-04 08:20:13 -0500

    Relocate strip_rmd.R

--- R/readme_from.R
+++ R/readme_from.R
@@ -1,38 +1,3 @@
-#' @example
-#' url <- "https://git.io/JU37u"
-#' out <- strip_rmd(url)
-#' cat(head(out), sep = "\n")
-#'
-#' # Compare
-#' cat(head(readLines(url)), sep = "\n")
-#' @noRd
-strip_rmd <- function(url) {
-  strip_setup(strip_yaml(readLines(url)))
-}
-
-strip_yaml <- function(lines) {
-  remove_lines_range(lines, untrim("---"), untrim("---"))
-}
-
-strip_setup <- function(lines) {
-  from <- untrim("```\\{r setup.*\\}")
-  to <- untrim("```")
-  remove_lines_range(lines, from, to)
-}
-
-untrim <- function(x) {
-  sprintf("^[ ]*%s[ ]*$", x)
-}
-
-remove_lines_range <- function(lines, from, to) {
-  .from <- grep(from, lines)[[1]]
-  to_candidates <- grep(to, lines)
-  .to <- to_candidates[to_candidates > .from][[1]]
-
-  useful <- setdiff(seq_along(lines), .from:.to)
-  lines[useful]
-}
-
 remove_lines <- function(lines, pattern) {
   lines <- grep(pattern, lines)[1:2]
   first <- lines[[2]] + 1L
